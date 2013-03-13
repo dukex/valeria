38839aa55e6844d70a2824ee0199bfe9d3eb3bc6
Duke <duke@riseup.net>
2013-03-13 03:38:32 -0300

    convert @url to url before fetch html

--- app/views/search.rb
+++ app/views/search.rb
@@ -4,10 +4,20 @@ require 'open-uri'
 module Views
   class Search < Mustache
     def images
-      parser = Nokogiri::HTML(open(@url).read)
+      parser = Nokogiri::HTML(open(@url.to_url).read)
       imgs = parser.search('img')
       imgs.map{|i| {url: i.attr('src')} }
     end
   end
 end
 
+
+class String
+  def to_url
+    url = self
+    unless url[/^http:\/\//] || url[/^https:\/\//]
+      url = 'http://' + url
+    end
+    url.downcase
+  end
+end
